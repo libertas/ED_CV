b713d83e5832ed550ae2a951873fe94176ffecbe
libertas <horizonvei@gmail.com>
2017-08-09 20:28:28 +0800

    add critical conditions for openmp

--- imageProcessing.cpp
+++ imageProcessing.cpp
@@ -7,11 +7,12 @@ using namespace cv;
 int centerOfMass(cv::Mat src, cv::Point &center)
 {
 	assert(src.channels() == 1);
-	int count = 0;
-	float xsum = 0;
-	float ysum = 0;
+	uint32_t count = 0;
+	uint32_t xsum = 0;
+	uint32_t ysum = 0;
 
-#pragma omp parallel for
+	#pragma omp parallel for num_threads(6)\
+		reduction(+:count) reduction(+:xsum) reduction(+:ysum)
 	for(int i = 0; i < src.rows; i++) {
 		for(int j = 0; j < src.cols; j++) {
 			if(src.at<uchar>(i, j) != 0) {
@@ -22,8 +23,13 @@ int centerOfMass(cv::Mat src, cv::Point &center)
 		}
 	}
 	
-	center.x = cvRound(xsum / count);
-	center.y = cvRound(ysum / count);
+	if(count != 0) {
+		center.x = xsum / count;
+		center.y = ysum / count;
+	} else {
+		center.x = 320;
+		center.y = 240;
+	}
 	
 	return 0;
 }
@@ -33,7 +39,7 @@ bool triThreshold(cv::Mat src, cv::Mat &dst, uint8_t highs[3], uint8_t lows[3])
 	if(src.channels() == 3) {
 		Mat tmp(src.rows, src.cols, CV_8U, Scalar(0));
 
-#pragma omp parallel for
+		#pragma omp parallel for num_threads(6)
 		for(int i = 0; i < src.rows; i++) {
 			for(int j = 0; j < src.cols; j++) {
 				uchar flag = 1;
@@ -45,9 +51,12 @@ bool triThreshold(cv::Mat src, cv::Mat &dst, uint8_t highs[3], uint8_t lows[3])
 					}
 				}
 
-				tmp.at<uchar>(i, j) = flag * 255;
+				#pragma omp critical
+				{
+					tmp.at<uchar>(i, j) = flag * 255;
+				}
 			}
-	}
+		}
 
 		dst = tmp;
 		return true;
